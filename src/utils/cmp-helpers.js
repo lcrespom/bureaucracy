5289ef232495e557a08f5f3c435d30d8d3d3d487
Luis Crespo <lcrespom@gmail.com>
2019-04-03 09:42:03 +0200

    Removed semicolons

--- src/utils/cmp-helpers.js
+++ src/utils/cmp-helpers.js
@@ -1,58 +1,58 @@
 export function validateTypes(cname, pname, prop, reqs) {
 	function reject(msg) {
-		console.error(`Error in property "${pname}" of component ${cname}: ${msg}`);
-		return false;
+		console.error(`Error in property "${pname}" of component ${cname}: ${msg}`)
+		return false
 	}
 	function typeOf(obj) {
-		return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1];
+		return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1]
 	}
 	for (let k of Object.keys(reqs)) {
 		if (reqs[k].required && !prop[k])
-			return reject(`property "${k}" is required`);
-		if (!prop[k]) continue;
+			return reject(`property "${k}" is required`)
+		if (!prop[k]) continue
 		if (reqs[k].type && typeOf(prop[k]) != reqs[k].type)
-			return reject(`property "${pname}.${k}" should be of type ${reqs[k].type}`);
+			return reject(`property "${pname}.${k}" should be of type ${reqs[k].type}`)
 	}
-	return true;
+	return true
 }
 
 export function getNestedField(obj, path) {
 	return path.split('.').reduce(
 		(prev, curr) => prev ? prev[curr] : undefined,
 		obj
-	);
+	)
 }
 
 export function setNestedField(obj, path, value) {
-	let parts = path.split('.');
-	let last = parts.pop();
+	let parts = path.split('.')
+	let last = parts.pop()
 	let parent = parts.reduce(
 		(prev, curr) => {
 			if (prev[curr] === undefined)
-				prev[curr] = {};
-			return prev[curr];
+				prev[curr] = {}
+			return prev[curr]
 		},
 		obj
-	);
-	if (parent) parent[last] = value;
+	)
+	if (parent) parent[last] = value
 }
 
 
 export function ucfirst(str) {
-	return str.charAt(0).toUpperCase() + str.slice(1);
+	return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
 export function wrapAsyncFunction(before, asyncF, after) {
 	return function(...params) {
-		before(...params);
+		before(...params)
 		return asyncF(...params)
 		.then(x => {
-			after(...params);
-			return x;
+			after(...params)
+			return x
 		})
 		.catch(x => {
-			after(...params);
-			throw x;
-		});
-	};
+			after(...params)
+			throw x
+		})
+	}
 }
--- src/utils/crud-api.js
+++ src/utils/crud-api.js
@@ -1,33 +1,33 @@
-import 'whatwg-fetch';
+import 'whatwg-fetch'
 
 const crudApi = {
 	getAll(apicfg, route) {
 		return fetch(getURL(apicfg, 'getAll', route))
-		.then(response => response.json());
+		.then(response => response.json())
 	},
 
 	post(apicfg, route, data) {
-		let url = getURL(apicfg, 'post', route);
+		let url = getURL(apicfg, 'post', route)
 		return fetch(url, fetchInit('POST', data))
-		.then(response => response.json());
+		.then(response => response.json())
 	},
 
 	put(apicfg, route, data) {
-		let url = getURL(apicfg, 'post', route);
+		let url = getURL(apicfg, 'post', route)
 		return fetch(url + '/' + data._id, fetchInit('PUT', data))
-		.then(response => response.json());
+		.then(response => response.json())
 	},
 
 	delete(apicfg, route, id) {
-		let url = getURL(apicfg, 'post', route);
+		let url = getURL(apicfg, 'post', route)
 		return fetch(url + '/' + id, { method: 'DELETE'})
-		.then(response => response.json());
+		.then(response => response.json())
 	}
-};
+}
 
 function getURL(apicfg, name, route) {
-	let suffix = apicfg[name] ? apicfg[name] : route;
-	return apicfg.prefix + suffix;
+	let suffix = apicfg[name] ? apicfg[name] : route
+	return apicfg.prefix + suffix
 }
 
 function fetchInit(method, data) {
@@ -37,7 +37,7 @@ function fetchInit(method, data) {
 		headers: new Headers({
 			'Content-Type': 'application/json'
 		})
-	};
+	}
 }
 
-export default crudApi;
+export default crudApi
